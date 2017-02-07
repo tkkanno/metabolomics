2d3020b2f57edd5f75b12702ea672befe442a21a
Tokuwa Kanno <tkkanno@gmail.com>
2017-02-07 11:06:41 +0000

    Update extract_bruker_titles.py

--- extract_bruker_titles.py
+++ extract_bruker_titles.py
@@ -6,23 +6,27 @@ directory  = str(raw_input("Enter directory of Bruker files: \n"))
 spectra = []
 datafiles = []
 numspectra = 0
-
+count = 0
+count2 = 0
 for dir, subdirs, files in os.walk(directory, followlinks = True):
   if '1r' in files:
     spectrumfile = dir + '/' + '1r'
     procsfile    = dir + '/' + 'procs'
     titlefile    = dir + '/' + 'title'
     datafiles.append(titlefile)
+    count+=1
 datafiles.sort()
 titles_list = []
 
 for titlefile in datafiles:
     if os.path.exists(titlefile):
+      count2+=1
       title = open(titlefile).read().strip()
     else:
       title = "<No title>"
     titles_list.append(title)
 
 titles_list = pd.Series(titles_list)
-
+print "Successfully copied %i/%i of the files found" %(count,count2)
 titles_list.to_csv(directory+'titles_list.csv')
+print "File saved to \n %s \n as 'titles_list.csv'" %directory
